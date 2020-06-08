f4fa49c5072b5f960c22f8d17a18c6c76adacf2c
Mateusz Leśniak <02mateuszlesniak02@gmail.com>
2020-06-08 09:10:51 +0200

    KalkulatorBMIv2
    BMI calculator made by me with using visual studio 2019 and C#.

---
+++ kalkulatorBMIv2SOLVE/kalkulatorBMIv2/Procesor.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace kalkulatorBMIv2
+{
+    class Procesor
+    {
+        
+        static public void GetSummary(string gender, double BMI)
+        {
+            if (gender == "K" || gender == "k")
+            {
+                Procesor.GetBMIWoman(BMI);
+            }
+            //Doing BMI maths for Man
+            if (gender == "M" || gender == "m")
+            {
+                Procesor.GetBMIMan(BMI);
+            }
+        }
+
+        static private void GetBMIWoman(double BMI)
+        {
+            if (BMI < 16)
+            {
+                Console.WriteLine("Jesteś wygłodzona.");
+            }
+            else if (BMI > 16.1 && BMI <= 17.0)
+            {
+                Console.WriteLine("Jesteś wychudzona.");
+            }
+            else if (BMI > 17.1 && BMI <= 18.5)
+            {
+                Console.WriteLine("Masz niedowagę.");
+            }
+            else if (BMI > 18.6 && BMI <= 25.0)
+            {
+                Console.WriteLine("Masz prawidłową wagę.");
+            }
+            else if (BMI > 25.1 && BMI <= 30.0)
+            {
+                Console.WriteLine("Masz nadwagę.");
+            }
+            else if (BMI > 30.1)
+            {
+                Console.WriteLine("Masz poważną nadwagę");
+            }
+        }
+        static private void GetBMIMan(double BMI)
+        {
+
+            if (BMI < 16)
+            {
+                Console.WriteLine("Jesteś wygłodzony.");
+            }
+            else if (BMI > 16.1 && BMI <= 17.0)
+            {
+                Console.WriteLine("Jesteś wychudzony.");
+            }
+            else if (BMI > 17.1 && BMI <= 18.5)
+            {
+                Console.WriteLine("Masz niedowagę.");
+            }
+            else if (BMI > 18.6 && BMI <= 25.0)
+            {
+                Console.WriteLine("Masz prawidłową wagę.");
+            }
+            else if (BMI > 25.1 && BMI <= 30.0)
+            {
+                Console.WriteLine("Masz nadwagę.");
+            }
+            else if (BMI > 30.1)
+            {
+                Console.WriteLine("Masz poważną nadwagę");
+            }
+        }
+  
+        static public int GetInformationFromUser()
+        {
+            while (true)
+            {
+                //kg value
+                
+
+                try
+                {
+                    return Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception e)
+                {
+
+                    Console.WriteLine("Źle podałeś wartość wzrostu, spróbuj podać ją w centymetrach.\n - Szczegóły dotyczące błędu: " + e.Message);
+
+                }
+            }
+        }
+
+        static public string GetSexFromUser(string gender)
+        {
+            while (true)
+            {
+                try
+                {
+                    return Convert.ToString(Console.ReadLine());
+                }
+                catch (Exception e)
+                {
+
+                    Console.WriteLine("Podałeś nieprawidłową wartość dla płci, wybierz pomiędzy K- kobieta i M- mężczyzna.\n - Szczegóły dotyczące błędu: " + e.Message);
+
+                }
+            }
+        }
+        static public void ShowResult(string gender, double BMI)
+        {
+            
+                if (gender == "M" || gender == "m")
+                {
+                    Console.WriteLine($"Pana BMI wynosi: {BMI}\n Co oznacza, że: \n");
+                }
+                else if (gender == "K" || gender == "k")
+                {
+                    Console.WriteLine($"Pani BMI wynosi: {BMI}\n Co oznacza, że: \n");
+                }
+        }
+        static public double BMI(double KG, double cm)
+        {
+            double BMI =  KG / Math.Pow(cm / 100.0, 2);
+            BMI = Math.Round(BMI, 2);
+            return BMI;
+        }
+    }
+}
---
+++ kalkulatorBMIv2SOLVE/kalkulatorBMIv2/Program.cs
@@ -0,0 +1,42 @@
+﻿using kalkulatorBMIv2;
+using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.Drawing;
+
+namespace KalkulatorBMIv2
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string gender = "";
+            bool endApp = false;
+            double KG = 0;
+            double cm = 0;
+            double BMI = KG / Math.Pow(cm / 100.0, 2);
+            Console.WriteLine("BMI calculator");
+            Console.WriteLine("---------------\n");
+
+            while (!endApp)
+            {
+                Console.Write("Podaj swoją wagę w kg: ");
+                KG = Procesor.GetInformationFromUser();
+                Console.Write("Podaj swój wzrost w cm: ");
+                cm = Procesor.GetInformationFromUser();
+                Console.Write("Podaj swoją płeć (K/M): ");
+                gender = Procesor.GetSexFromUser(gender);
+                BMI = Procesor.BMI(KG, cm);
+                Procesor.ShowResult(gender, BMI);
+                Procesor.GetSummary(gender, BMI);
+                Console.WriteLine("------------------------\n");
+                Console.Write("Naciśnij 'n' i Enter żeby zamknąć aplikację, albo inny dowolny guzik i enter żeby kontynuować: ");
+                if (Console.ReadLine() == "n") endApp = true;
+                Console.WriteLine("\n");
+
+            }
+            return;
+        }
+    }
+}
